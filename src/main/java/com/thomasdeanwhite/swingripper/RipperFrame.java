425d0a35b02ff7ca75903274d2128f1e75bb867a
Thomas White <tdwhite1@sheffield.ac.uk>
2018-02-06 18:54:43 +0000

    Swing GUIs are successfully being ripped clean

--- src/main/java/com/thomasdeanwhite/swingripper/RipperFrame.java
+++ src/main/java/com/thomasdeanwhite/swingripper/RipperFrame.java
@@ -17,6 +17,28 @@ public class RipperFrame extends JFrame {
 
     public static final String TITLE = "com.thomasdeanwhite.swingripper.SwingRipper - v0.0.1";
 
+    private static HashMap<Class, String> CLASS_MAP = new HashMap<>();
+
+    static {
+        CLASS_MAP.put(JButton.class, "button");
+        CLASS_MAP.put(JTextField.class, "text_field");
+        CLASS_MAP.put(JTextArea.class, "text_field");
+        CLASS_MAP.put(JComboBox.class, "combo_box");
+        CLASS_MAP.put(JToggleButton.class, "toggle_button");
+        CLASS_MAP.put(JMenu.class, "menu");
+        CLASS_MAP.put(JMenuItem.class, "menu_item");
+        CLASS_MAP.put(JList.class, "list");
+        CLASS_MAP.put(JTree.class, "tree");
+
+        try {
+            Class scrollbarClass = Class.forName("javax.swing.JScrollPane$ScrollBar");
+            CLASS_MAP.put(scrollbarClass, "scroll_bar");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static String DEFAULT_DATA_DIR = "~/data/";
 
     public static HashMap<Integer, Component> components;
@@ -31,6 +53,8 @@ public class RipperFrame extends JFrame {
     JButton paint = new JButton("Paint");
 
     JButton rip = new JButton("Rip");
+
+    JLabel status = new JLabel("Ready");
 
     JTextField dataLocation = new JTextField();
 
@@ -62,14 +86,22 @@ public class RipperFrame extends JFrame {
         refresh.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                status.setText("Updating...");
+                refresh();
                 RipperFrame.this.updateWindows();
+                status.setText("Updating... Done!");
+                refresh();
             }
         });
 
         paint.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                status.setText("Painting...");
+                refresh();
                 RipperFrame.this.highlightComponent();
+                status.setText("Painting... Done!");
+                refresh();
             }
         });
 
@@ -77,7 +109,11 @@ public class RipperFrame extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
+                    status.setText("Ripping...");
+                    refresh();
                     RipperFrame.this.rip();
+                    status.setText("Ripping... Done!");
+                    refresh();
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
@@ -91,6 +127,8 @@ public class RipperFrame extends JFrame {
         actionContainer.add(paint);
 
         actionContainer.add(rip);
+
+        actionContainer.add(status);
 
         add(actionContainer, BorderLayout.SOUTH);
 
@@ -128,37 +166,116 @@ public class RipperFrame extends JFrame {
 
     }
 
+    public void recursiveParse(Component c, int[] positions, int imageId, Statement stmt) throws SQLException {
 
-    public void rip() throws SQLException{
+        if(c instanceof Container) {
+            Container conts = ((Container) c);
+
+            for (Component cont : conts.getComponents()) {
+                try {
+                    recursiveParse(cont, positions, imageId, stmt);
+                } catch (IllegalComponentStateException e){
+                    //component is invisible!
+                }
+            }
+        }
+
+        if (c.getWidth() == 0 || c.getHeight() == 0 || !c.isVisible() || !c.isDisplayable() ||
+                !c.isEnabled()){
+            return;
+        }
+
+        String labelType = null;
+
+        for (Class cls : CLASS_MAP.keySet()){
+            if (cls.isInstance(c)){
+                labelType = CLASS_MAP.get(cls);
+            }
+        }
+
+        if (labelType == null){
+            return;
+        }
+
+        if (c instanceof JTextField){
+            if (!((JTextField)c).isEditable()){
+                return;
+            }
+        }
+
+        float[] dimensions = new float[]{
+                (c.getLocationOnScreen().x-positions[0]-4)/(float)positions[2],
+                (c.getLocationOnScreen().y-positions[1]-4)/(float)positions[3],
+                ((c.getLocationOnScreen().x-positions[0]+8) + c.getWidth())/(float)positions[2],
+                ((c.getLocationOnScreen().y-positions[1]+8) + c.getHeight())/(float)positions[3],
+        };
+
+        ResultSet rs=stmt.executeQuery(String.format("SELECT LabelTypeId FROM label_types WHERE LabelName='%s'", labelType));
+
+        int labelTypeId = 0;
+
+        if (rs.next()){
+            labelTypeId = rs.getInt(1);
+        } else {
+            labelTypeId = stmt.executeUpdate(String.format("INSERT INTO label_types (LabelName) VALUES ('%s');", labelType));
+
+            rs=stmt.executeQuery(String.format("SELECT LabelTypeId FROM label_types WHERE LabelName='%s'", labelType));
+
+            if (rs.next()) {
+                labelTypeId = rs.getInt(1);
+            }
+        }
+
+        stmt.executeUpdate(String.format("INSERT INTO labels (Source, Confidence, ImageId, " +
+                        "XMin, XMax, YMin, YMax, LabelType) VALUES ('%s', '%d', '%d', '%f', '%f', '%f', '%f'," +
+                        "'%d');", "unverified", 0, imageId, dimensions[0], dimensions[2], dimensions[1],
+                dimensions[3], labelTypeId));
+
+    }
+
+
+    public void rip() throws SQLException {
         Connection con = DriverManager.getConnection(
                     "jdbc:mysql://" + Preferences.LOCATION + ":" + Preferences.PORT + "/" + Preferences.DB,
-                    Preferences.USER,Preferences.PASSWORD);
+                    Preferences.USER, Preferences.PASSWORD);
 
         String selected = getSelected().toString();
         Window[] windows = Window.getWindows();
         for (Window w : windows) {
-            JFrame frame = (JFrame)SwingUtilities.getRoot(w);
+            String title = "";
 
-            if (w.isVisible() && frame.getTitle().equals(selected)) {
-                // rip the selected gui
-                frame.toFront();
+            // parse window
+            Container cont = (Container)SwingUtilities.getRoot(w);
 
-                frame.repaint();
+            if (cont instanceof JFrame){
+                title = ((JFrame)cont).getTitle();
+            } else if (cont instanceof JOptionPane){
+                title = ((JOptionPane)cont).getMessage().toString();
+            } else if (cont instanceof JDialog){
+                title = ((JDialog)cont).getTitle();
+            }
+
+            title = SwingRipper.JAR_FILE + " - " + title;
+
+            if (w.isVisible() && title.equals(selected)) {
+
+                cont.repaint();
 
                 try {
                     Robot robot = new Robot();
 
-                    Point pos = frame.getLocationOnScreen();
+                    Point pos = cont.getLocationOnScreen();
 
-                    int[] positions = new int[]{pos.x, pos.y, frame.getWidth(), frame.getHeight()};
+                    int[] positions = new int[]{pos.x, pos.y, cont.getWidth(), cont.getHeight()};
 
                     BufferedImage screenshot = robot.createScreenCapture(new Rectangle(positions[0], positions[1],
                             positions[2], positions[3]));
 
-                    String source = "localhost?application=" + frame.getTitle().replace(" ", "_");
+                    String source = "localhost?application=" + title.replace(" ", "_").
+                            replace(".", "");
 
                     Statement stmt=con.createStatement();
-                    ResultSet rs=stmt.executeQuery(String.format("SELECT * FROM images WHERE Source='%s'", "lol"));
+                    ResultSet rs=stmt.executeQuery(String.format("SELECT * FROM images WHERE Source='%s'", source));
 
                     boolean exists = false;
 
@@ -170,9 +287,9 @@ public class RipperFrame extends JFrame {
 
                     float num = (float)Math.random();
 
-                    if (num < 0.15){
+                    if (num < 0.1){
                         dataset = "train";
-                    } else if (num < 0.05) {
+                    } else if (num < 0.15) {
                         dataset = "validate";
                     }
 
@@ -180,15 +297,20 @@ public class RipperFrame extends JFrame {
 
                         int maxImage = 0;
 
-                        rs=stmt.executeQuery(String.format("SELECT * FROM images ORDER BY ImageID DESC"));
+                        rs=stmt.executeQuery(String.format("SELECT * FROM images ORDER BY ImageID DESC;"));
 
-                        maxImage = rs.getInt(0);
+                        rs.next();
 
-                        String filename = "images/" + (maxImage+1) + ".png";
+                        maxImage = rs.getInt(1);
 
-                        rs=stmt.executeQuery(String.format("INSERT INTO images (Subset, File, Width, Height, Source) " +
+                        int imageId = (maxImage+1);
+
+                        String filename = "images/" + imageId + ".png";
+
+                        stmt.executeUpdate(String.format("INSERT INTO images (Subset, File, Width, Height, Source) " +
                                 "VALUES ('%s', '%s', '%d', '%d', '%s');", dataset, filename,
                                 screenshot.getWidth(), screenshot.getHeight(), source));
+
 
                         String output = dataLocation.getText();
 
@@ -199,6 +321,12 @@ public class RipperFrame extends JFrame {
                         output += filename;
 
                         ImageIO.write(screenshot, "png", new File(output));
+
+
+                        for (Component c : cont.getComponents()){
+
+                            recursiveParse(c, positions, imageId, stmt);
+                        }
                     }
                 } catch (AWTException e) {
                     e.printStackTrace();
@@ -246,14 +374,25 @@ public class RipperFrame extends JFrame {
 
         for (Window w : windows){
             if (w.isVisible()){
-                // parse window
-                JFrame frame = (JFrame)SwingUtilities.getRoot(w);
 
-                if (!frame.getTitle().equals(TITLE)) {
-                    DefaultMutableTreeNode window = new DefaultMutableTreeNode(frame.getTitle());
+                String title = "";
+
+                // parse window
+                Container cont = (Container)SwingUtilities.getRoot(w);
+
+                if (cont instanceof JFrame){
+                    title = ((JFrame)cont).getTitle();
+                } else if (cont instanceof JOptionPane){
+                    title = ((JOptionPane)cont).getMessage().toString();
+                } else if (cont instanceof JDialog){
+                    title = ((JDialog)cont).getTitle();
+                }
+
+                if (!title.equals(TITLE)) {
+                    DefaultMutableTreeNode window = new DefaultMutableTreeNode(SwingRipper.JAR_FILE + " - " + title);
                     root.add(window);
 
-                    for (Component c : frame.getComponents()){
+                    for (Component c : cont.getComponents()){
 
                         recursiveSearch(c, window);
                     }
@@ -261,7 +400,5 @@ public class RipperFrame extends JFrame {
                 }
             }
         }
-
-        refresh();
     }
 }
--- src/main/java/com/thomasdeanwhite/swingripper/SwingRipper.java
+++ src/main/java/com/thomasdeanwhite/swingripper/SwingRipper.java
@@ -1,5 +1,6 @@
 package com.thomasdeanwhite.swingripper;
 
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -7,11 +8,15 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 public class SwingRipper {
+
+    public static String JAR_FILE = "";
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -20,6 +25,8 @@ public class SwingRipper {
         String jar = args[0];
 
         File path = new File(jar);
+
+        JAR_FILE = jar.substring(jar.lastIndexOf("/")+1);
 
         if (!path.exists()) {
             throw new IllegalArgumentException("args[0] should be a valid jar file");
@@ -77,6 +84,50 @@ public class SwingRipper {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+
+        String input = "";
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!(input=scanner.nextLine()).equals("q")){
+            try {
+                String[] command = input.split(" ");
+
+                String cmd = command[0];
+
+                String param = "";
+
+                if (command.length > 1) {
+                    param = command[1];
+                }
+
+                switch (cmd) {
+                    case "refresh":
+                        ripperFrame.updateWindows();
+                        break;
+                    case "rip":
+                        try {
+                            ripperFrame.rip();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "select":
+                        ripperFrame.frames.getSelectionModel().clearSelection();
+                        try {
+                            int row = Integer.parseInt(param);
+                            ripperFrame.frames.addSelectionRow(row);
+                            ripperFrame.frames.expandRow(row);
+                        } catch (NumberFormatException e) {
+                            System.err.println(e.getMessage());
+                        }
+                }
+
+                ripperFrame.refresh();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
