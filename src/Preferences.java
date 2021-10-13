e66bd7e48379498024fffc4c500c10388da77a40
Thomas D. White <thomas.w@spotqa.com>
2021-10-13 11:28:22 +0100

    Fix preferences file

--- src/Preferences.java
+++
@@ -1,7 +0,0 @@
-public class Preferences {
-    public static final String LOCATION = "localhost";
-    public static final int PORT = 3306;
-    public static final String USER = "***USERNAME***";
-    public static final String PASSWORD = "***PASSWORD***";
-    public static final String DB = "gui_images";
-}
