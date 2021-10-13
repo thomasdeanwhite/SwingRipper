bb3530c6bc2acb815478290798975298ffca9ad0
Thomas D. White <thomas.w@spotqa.com>
2021-10-13 11:27:44 +0100

    Fix preferences file

--- src/main/java/com/thomasdeanwhite/swingripper/Preferences.java
+++ src/main/java/com/thomasdeanwhite/swingripper/Preferences.java
@@ -3,7 +3,7 @@ package com.thomasdeanwhite.swingripper;
 public class Preferences {
     public static final String LOCATION = "localhost";
     public static final int PORT = 3306;
-    public static final String USER = "root";
-    public static final String PASSWORD = "my-db-password";
-    public static final String DB = "guiimages";
+    public static final String USER = "***USERNAME***";
+    public static final String PASSWORD = "***PASSWORD***";
+    public static final String DB = "gui_images";
 }
